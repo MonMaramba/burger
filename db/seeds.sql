e61338a0e3c1767b2e6957a39bf3ae6260c87e6d
unknown <monmaramba@yahoo.com>
2018-08-19 15:32:31 -0700

    db folder complete, database created thru cmdline

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Quarter Pounder with Cheese", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Ramen Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Krusty Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Burger of the Gods", false);

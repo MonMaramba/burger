f4e849ed513683b8c70d71777695a4478c38d48c
unknown <monmaramba@yahoo.com>
2018-08-23 18:03:52 -0700

    orm, updateOne is correct

--- config/orm.js
+++ config/orm.js
@@ -2,14 +2,14 @@ var connection = require("./connection.js");
 
 var orm = {
     selectAll: function(whatToSelect, tableInput){
-        var queryString = "SELECT * FROM burgers";
-        connection.query(queryString, function(err, result){
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [whatToSelect, tableInput], function(err, result){
             if (err) throw err;
             console.log(result);
         });
     },
     insertOne: function(tableInput, colToSearch, valOfCol) {
-        var queryString = "SELECT ?? FROM burgers WHERE ??";
+        var queryString = "SELECT ?? FROM ?? WHERE ??";
 
         console.log(queryString);
 
@@ -17,6 +17,21 @@ var orm = {
             if (err) throw err;
             console.log(result);
         });
-        updateOne()
+        updateOne: function(tableInput, colToSearch, valOfCol) {
+            var queryString = "UPDATE" + table;
+
+            queryString += "SET";
+            queryString += objToSql(objColVals);
+            queryString += "WHERE";
+            queryString += condition;
+
+            console.log(queryString)
+            connection.query(queryString, function(err, result){
+                if (err) {
+                    throw err;
+                }
+                cb(result);
+            })
+        }
     }
 }

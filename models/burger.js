7a665d2aa73b5be0e55f0286c3d33083d693e4ae
unknown <monmaramba@yahoo.com>
2018-08-24 22:00:29 -0700

    burger controller done

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,7 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-selectAll: function(cb) {
+all: function(cb) {
         orm.selectAll("burgers", function(res){
             cb(res);
         });
@@ -15,7 +15,7 @@ selectAll: function(cb) {
     },
 
     updateOne: function(objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function(res){
+        orm.updateOne("burgers", objColVals, condition, function(res){
             cb(res);
         });
     }

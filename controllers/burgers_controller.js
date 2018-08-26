6133da20a96ba3980122ddbbb381eb920e1f7a32
unknown <monmaramba@yahoo.com>
2018-08-25 18:02:24 -0700

    all folders

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,11 +16,11 @@ router.get("/", function(req, res){
 
 router.post("/api/burgers", function(req, res){
     burger.insertOne([
-        "burgerName", "devoured"
+        "burger_name", "devoured"
     ], [
-        req.body.burgerName, req.body.burgerName
-    ], function(result) {
-        res.json({ id: result.insertId});
+        req.body.burger_name,
+    ], function(res) {
+        res.json({ id: res.insertId});
     });
 });
 

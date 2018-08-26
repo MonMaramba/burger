94cce4fba1848fc1f7df16dafba34164ab946ed5
unknown <monmaramba@yahoo.com>
2018-08-25 22:56:03 -0700

    pw removed

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "DamnSQL1!",
+    password: "",
     database: "burgers_db"
 });
 

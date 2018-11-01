7c89e90efe57b4d4b3008a63a66a0ccb8d4e8e49
unknown <monmaramba@yahoo.com>
2018-10-31 21:10:12 -0700

    setup corrections

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
-//dependency
+//dependency for MySQL connection
 var mysql = require("mysql");
+
 var connection;
 if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+
     connection = mysql.createConnection({
         host: "localhost",
     port: 3306,
@@ -13,7 +15,13 @@ if (process.env.JAWSDB_URL){
     })
 }
 
-
-connection.connect();
+// To connect
+connection.connect(function(err){
+    if(err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadID);
+});
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -5,14 +5,17 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+//to serve static content for the app from the public directory
+app.use(express.static("./public"));
 
-app.use(express.static("public"));
-
+//To parse application from  www-form-url-encoded
 app.use(bodyParser.urlencoded({ extended: true}));
-
+// To parse json objects
 app.use(bodyParser.json());
 
+// handlebars setup
 var exphbs = require("express-handlebars");
+
 
 app.engine("handlebars", exphbs({ dafaultLayout: "main"}));
 app.set("view engine", "handlebars");
@@ -21,6 +24,8 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
+//To start the server and listen to client requests
 app.listen(PORT, function(){
+    // Server-side logging for when server starts
     console.log("Server listening on: http://localhost: " + PORT);
 });

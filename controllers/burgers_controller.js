66b76bdc643d9fdc70e7bd4d80616545783760c0
unknown <monmaramba@yahoo.com>
2018-10-31 22:06:37 -0700

    tweaks and fixes. Model schema is the same

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,8 +4,8 @@ var burger = require("../models/burger.js");
 
 var router = express.Router();
 
-router.get("/", function(req, res){
-    burger.selectAll(function(data){
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
         var hbsObject = {
             burgers: data
         };
@@ -14,24 +14,33 @@ router.get("/", function(req, res){
     });
 });
 
-router.post("/api/burgers", function(req, res){
+router.post("/api/burgers", function (req, res) {
     console.log(req.body.burger_name);
-    burger.insertOne(req.body.burger_name, function(result){
-        res.json({ id: result.insertId});
-    })
-    
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+            req.body.burger_name, req.body.devoured
+        ], function (result) {
+            // Send back the ID of the new quote
+            res.json({ id: result.insertId });
+        });
 });
 
-router.put("/api/burger/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
     burger.updateOne({
         devoured: req.body.devoured
-    }, condition, function(){
-       res.redirect("/");
-    })
-})
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, retun a 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router;
